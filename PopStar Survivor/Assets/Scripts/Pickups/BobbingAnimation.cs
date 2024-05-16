f07a4ac6ea834dfb8c5c55dabd0c815e3e2fc0ec
DavidGarciaOriol <davidgarciaoriol@gmail.com>
2024-05-16 16:07:09 +0200

    Correcciones
     - Corregidos algunos errores referentes a recolectar objetos.
     - Corregida interfaz.
     - Correcciones menores.
     - Build actualizada.

--- PopStar Survivor/Assets/Scripts/Pickups/BobbingAnimation.cs
+++ PopStar Survivor/Assets/Scripts/Pickups/BobbingAnimation.cs
@@ -7,18 +7,24 @@ public class BobbingAnimation : MonoBehaviour
     public float frequency; // Velocidad del movimiento.
     public float magnitude; // Rango del movimiento.
     public Vector3 direction; // Dirección del movimiento.
+    Pickup pickup;
 
     Vector3 initialPosition;
 
     void Start()
     {
+        pickup = GetComponent<Pickup>();
         initialPosition = transform.position;
     }
 
     void Update()
     {
-        // Función para el efecto de tembleque/bobbing
-        transform.position = initialPosition + direction * Mathf.Sin(Time.time * frequency) * magnitude;
+        if (pickup && !pickup.hasBeenCollected) 
+        {
+            // Función para el efecto de tembleque/bobbing
+            transform.position = initialPosition + direction * Mathf.Sin(Time.time * frequency) * magnitude;
+        }
+        
 
     }
 }
